ea316d22477721d8e0ede168161f05351a46d96c
Ethan Fairweather <Ethan-Fairweather@Inunotaishos-MacBook-Pro.local>
2017-07-23 12:35:03 -0400

    pushing navlink and route literals

--- 3-Introducing-the-Project/course-directory/src/components/App.js
+++ 3-Introducing-the-Project/course-directory/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Header/>
+      <Route exact path="/" render={()=> <Home/>}/>
+      <Route path="/about" render={ () => <About title ='About'/>}/>
+      <Route path="/teachers" render={ () => <Teachers/>}/>
+      <Route path="/courses" render={() => <Courses/>}/>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
